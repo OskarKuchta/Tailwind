ac9bd5f32632aec1f862fcb80a23ff193e2cd5d3
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-07-07 12:26:32 +0200

    Create React list item and add some tailwind styles

--- App/src/components/ListWithInputs.tsx
+++ App/src/components/ListWithInputs.tsx
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ListWithInputs: React.FC = () => {
-  const list = [
+  const [value, setValue] = useState("");
+  const ul: string[] = [
     "some values",
     "to create",
     "a unordered",
     "list to test",
     "and repeat knowledge",
   ];
+  const [list, setList] = useState<string[]>(ul);
+  const grabItem = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+  const addItem: () => void = () => {
+    if (value.trim() === "") {
+      return;
+    }
+    setList((prev) => [...prev, value]);
+    setValue("");
+  };
+  const removeItem: () => void = () => {
+    const updatedList = [...list];
+    updatedList.pop();
+    setList(updatedList);
+  };
   return (
-    <section className="my-10">
-      <ol className="ordered-list">
+    <section className="my-10 flex flex-col">
+      <ul className="ordered-list">
         {list.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index} className="text-[#445dd8]">
+            {item}
+          </li>
         ))}
-      </ol>
-      <input type="text" />
+      </ul>
+      <input
+        id="list-item"
+        value={value}
+        type="text"
+        className="placeholder-[#445dd8] text-[#445dd8]"
+        placeholder="Type someone"
+        onChange={grabItem}
+      />
       <div className="flex justify-between">
-        <button className="mt-5 bg-green-500 py-3 px-7 rounded">Add</button>
-        <button className="mt-5 bg-red-600 py-3 px-7 rounded">Remove</button>
+        <button
+          className="mt-5 bg-add py-3 px-3 lg:px-7 rounded focus:outline-add focus:scale-110"
+          onClick={addItem}
+        >
+          Add
+        </button>
+        <button
+          className="mt-5 bg-remove py-3 px-3 lg:px-7 rounded focus:outline-remove focus:scale-110"
+          onClick={removeItem}
+        >
+          Remove
+        </button>
       </div>
     </section>
   );
