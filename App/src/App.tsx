80765063370e08def2060e69f6b0c6cb5c8c741d
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-07-11 20:31:22 +0200

    Change working footer and set fancy hover effect on it

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 const App: React.FC = () => {
   return (
     <>
-      <div className="w-3/4 flex flex-col mx-auto">
+      <div className="w-3/4 flex flex-col mx-auto min-h-screen mb-10">
         <main className="flex flex-col items-center w-4/5 sm:w-2/3 md:w-1/3 mx-auto">
           <Header />
           <Photos />
--- App/src/components/Footer.tsx
+++ App/src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="sticky bg-black w-screen text-white object-contain">
-      <p className="px-6 py-2 text-center text-sm sm:text-base md:text-lg">
+    <footer className="fixed bg-black text-white object-contain w-screen">
+      <p className="px-6 py-2 text-center text-sm sm:text-base md:text-lg footer-text">
         2023 Your Website &copy; <span className="mr-2"></span>
         All rights reserved. | Made with Tailwind CSS
       </p>
