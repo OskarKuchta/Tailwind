387ac2496f8c1e3324cf029fba4ab204a7f845cc
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-07-12 16:20:39 +0200

    Create different footer reason for mobile and desktop

--- App/src/components/Footer.tsx
+++ App/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="fixed bg-black text-white object-contain w-screen">
+    <footer className="fixed md:sticky bg-black text-white w-screen">
       <p className="px-6 py-2 text-center text-sm sm:text-base md:text-lg footer-text">
         2023 Your Website &copy; <span className="mr-2"></span>
         All rights reserved. | Made with Tailwind CSS
